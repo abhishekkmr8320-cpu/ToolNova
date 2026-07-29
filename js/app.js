e678ff2702ccb7a3cd4b8b63890e44a8f33ce557
abhishekkmr8320-cpu <abhishekkmr8320@gmail.com>
2026-07-29 18:57:44 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+// ToolNova
 
+const searchInput = document.querySelector(".search-box input");
+const cards = document.querySelectorAll(".card");
+
+if (searchInput) {
+    searchInput.addEventListener("keyup", function () {
+
+        let value = this.value.toLowerCase();
+
+        cards.forEach(card => {
+
+            let text = card.textContent.toLowerCase();
+
+            if (text.includes(value)) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+}
+
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        alert(card.innerText + " - Coming Soon");
+
+    });
+
+});
